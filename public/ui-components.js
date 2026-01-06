b50c963b405efb2dd9267ed760d0ee338f658051
Jens Keller <keller_jens@hotmail.com>
2026-01-06 23:31:36 +0800

    make connectors more visual

--- public/ui-components.js
+++ public/ui-components.js
@@ -3,9 +3,10 @@ import { html, useLayoutEffect, useMemo } from 'preact-standalone'
 const FONT_FAMILY = 'Space Grotesk, sans-serif'
 const fillColor = (active) => active ? '#22c55e' : '#f87171'
 const lineColor = (active) => active ? '#047857' : '#b91c1c'
+const inputPinRadius = 3
+const outputPinRadius = 2
 
 // REVISE if we send relative positions for pins, we don't need to update the registry on every drag
-
 const useRegisterPins = (id, pinRegistry, pins) => {
     useLayoutEffect(() => {
         pinRegistry.set(id, pins)
@@ -29,6 +30,7 @@ export const ToggleNode = ({ id, label, position, active, pinRegistry }) => {
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
                 <rect width=${trackW} height=${trackH} rx=${trackR} ry=${trackR} />
                 <line x1="40" y1="10" x2="50" y2="10" />
+                <circle stroke-width="0" cx="50" cy="10" r=${outputPinRadius} />
                 <circle cx=${knobX} cy=${trackR} r=${knobR} fill="#f8fafc" />
                 <text style="user-select: none;" x=${knobX} y=${trackR + 1} text-anchor="middle" dominant-baseline="middle" font-family=${FONT_FAMILY} font-size="10" stroke-width="1">
                     ${active ? '✔' : 'X'}
@@ -39,13 +41,13 @@ export const ToggleNode = ({ id, label, position, active, pinRegistry }) => {
 }
 
 export const NotGateNode = ({ id, label, position, active, pinRegistry }) => {
-    const inputY = 10, startX = 0, height = 20, tipX = 20, bubbleRadius = 5
+    const pinY = 10, startX = 0, height = 20, tipX = 20, bubbleRadius = 5
     const bubbleCenterX = tipX + bubbleRadius
     const outputX = bubbleCenterX + bubbleRadius + 10
 
     const pins = useMemo(() => [
-        { id: 'in', x: position.x - 10, y: position.y + inputY },
-        { id: 'out', x: position.x + outputX, y: position.y + inputY }
+        { id: 'in', x: position.x - 10, y: position.y + pinY },
+        { id: 'out', x: position.x + outputX, y: position.y + pinY }
     ], [position.x, position.y])
 
     useRegisterPins(id, pinRegistry, pins)
@@ -54,10 +56,12 @@ export const NotGateNode = ({ id, label, position, active, pinRegistry }) => {
         <g transform=${`translate(${position.x} ${position.y})`}>
             <text y="-8" text-anchor="left" font-family=${FONT_FAMILY} font-size="12" font-weight="400">${label}</text>
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
-                <path d=${`M${startX} 0 L${startX} ${height} L${tipX} ${inputY} Z`} />
-                <line x1="-10" y1=${inputY} x2=${startX} y2=${inputY} />
-                <circle cx=${bubbleCenterX} cy=${inputY} r=${bubbleRadius} />
-                <line x1=${bubbleCenterX + bubbleRadius} y1=${inputY} x2=${outputX} y2=${inputY} />
+                <path d=${`M${startX} 0 L${startX} ${height} L${tipX} ${pinY} Z`} />
+                <circle cx=${bubbleCenterX} cy=${pinY} r=${bubbleRadius} />
+                <line x1="-10" y1=${pinY} x2=${startX} y2=${pinY} />
+                <circle stroke-width="0" cx=-10 cy=${pinY} r=${inputPinRadius} />
+                <line x1=${bubbleCenterX + bubbleRadius} y1=${pinY} x2=${outputX} y2=${pinY} />
+                <circle stroke-width="0" cx=${outputX} cy=${pinY} r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -78,8 +82,11 @@ export const AndGateNode = ({ id, label, position, active, pinRegistry }) => {
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
                 <path d="M 0 0 L 20 0 A 10 10 0 0 1 20 40 L 0 40 L 0 0 Z" />
                 <line x1="-10" y1="10" x2="0" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="0" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <line x1="40" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -100,8 +107,11 @@ export const OrGateNode = ({ id, label, position, active, pinRegistry }) => {
             <g fill=${fillColor(active)} stroke=${lineColor(active)}>
                 <path d="M 0 0 L 20 0 A 10 10 0 0 1 20 40 L 0 40 A 6 10 0 0 0 0 0 Z" />
                 <line x1="-10" y1="10" x2="10" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="10" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <line x1="40" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -122,9 +132,12 @@ export const NandGateNode = ({ id, label, position, active, pinRegistry }) => {
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
                 <path d="M 0 0 L 15 0 A 15 15 0 0 1 15 40 L 0 40 L 0 0 Z" />
                 <line x1="-10" y1="10" x2="0" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="0" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <circle cx="40" cy="20" r="5" />
                 <line x1="45" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -145,9 +158,12 @@ export const NorGateNode = ({ id, label, position, active, pinRegistry }) => {
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
                 <path d="M 0 0 L 15 0 A 15 15 0 0 1 15 40 L 0 40 A 6 10 0 0 0 0 0 Z" />
                 <line x1="-10" y1="10" x2="10" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="10" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <circle cx="40" cy="20" r="5" />
                 <line x1="45" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -169,8 +185,11 @@ export const XorGateNode = ({ id, label, position, active, pinRegistry }) => {
                 <path d="M 0 0 L 20 0 A 10 10 0 0 1 20 40 L 0 40 A 6 10 0 0 0 0 0 Z" />
                 <path d="M -5 3 A 6 10 0 0 1 -5 37" fill="none" />
                 <line x1="-10" y1="10" x2="3" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="3" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <line x1="40" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -192,9 +211,12 @@ export const XnorGateNode = ({ id, label, position, active, pinRegistry }) => {
                 <path d="M 0 0 L 15 0 A 15 15 0 0 1 15 40 L 0 40 A 6 10 0 0 0 0 0 Z" />
                 <path d="M -5 3 A 6 10 0 0 1 -5 37" fill="none" />
                 <line x1="-10" y1="10" x2="3" y2="10" />
+                <circle stroke-width="0" cx="-10" cy="10" r=${inputPinRadius} />
                 <line x1="-10" y1="30" x2="3" y2="30" />
+                <circle stroke-width="0" cx="-10" cy="30" r=${inputPinRadius} />
                 <circle cx="40" cy="20" r="5" />
                 <line x1="45" y1="20" x2="50" y2="20" />
+                <circle stroke-width="0" cx="50" cy="20" r=${outputPinRadius} />
             </g>
         </g>
     `
@@ -215,6 +237,7 @@ export const DisplayProbeNode = ({ id, label, position, active, pinRegistry }) =
             <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="2">
                 <rect width=${width} height=${height} rx=${borderRadius} ry=${borderRadius} />
                 <line x1="-10" y1=${height / 2} x2="0" y2=${height / 2} />
+                <circle stroke-width="0" cx="-10" cy=${height / 2} r=${inputPinRadius} />
                 <text style="user-select: none;" x=${width / 2} y=${height / 2} text-anchor="middle" dominant-baseline="middle" font-family=${FONT_FAMILY} font-size="14" stroke-width="2">
                     ${active ? 1 : 0}
                 </text>
@@ -227,11 +250,11 @@ export const WirePath = ({ from, sourcePin, to, targetPin, active }) => {
     const points = [from, to].map((p) => `${p.x},${p.y}`).join(' ')
     
     return html`
-        <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="0">
+        <g fill=${fillColor(active)} stroke=${lineColor(active)} stroke-width="1">
             <polyline points=${points} fill="none" stroke="white" opacity="0" stroke-width="10"/>
             <polyline points=${points} fill="none" stroke-width=${active ? 3 : 2} />
-            <circle fill=${!sourcePin ? 'black' : lineColor(active)} cx=${from.x} cy=${from.y} r="2.5" />
-            <circle fill=${!targetPin ? 'black' : lineColor(active)} cx=${to.x} cy=${to.y} r="2.5" />
+            <circle fill=${!sourcePin ? 'white' : lineColor(active)} stroke=${!sourcePin ? 'black' : lineColor(active)} cx=${from.x} cy=${from.y} r=${outputPinRadius} />
+            <circle fill=${!targetPin ? 'white' : lineColor(active)} stroke=${!targetPin ? 'black' : lineColor(active)} cx=${to.x} cy=${to.y} r=${inputPinRadius} />
         </g>
     `
 }
